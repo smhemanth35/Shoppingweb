8fa84887cad383daa1f45de8b96fb4e942a032f1
HEMANTH S M <149108173+smhemanth35@users.noreply.github.com>
2024-02-09 15:57:17 +0530

    Add files via upload

---
+++ shopping-cart-main/javascript/main.js
@@ -0,0 +1,222 @@
+const toggler = document.querySelector(".slider");
+const cartModal = document.querySelector(".cart-modal");
+const cartIcon = document.querySelector(".cartIcon");
+const cartBtn = document.querySelector(".cartBtn");
+const backDrop = document.querySelector(".backdrop");
+const confirmBtn = document.querySelector(".cart-item-confirm");
+const productsParent = document.querySelector(".main-child");
+const cartItems = document.querySelector(".counter");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const clearCart = document.querySelector(".clear-cart");
+
+import { productsData } from "./products.js";
+
+let cart = [];
+let domButtons = [];
+
+toggler.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
+
+cartIcon.addEventListener("click", showFunc);
+
+cartBtn.addEventListener("click", showFunc);
+
+function showFunc() {
+  backDrop.style.display = "block";
+  cartModal.style.transform = "translateY(40vh)";
+  cartModal.style.opacity = "1";
+}
+
+backDrop.addEventListener("click", closeFunc);
+
+confirmBtn.addEventListener("click", closeFunc);
+
+function closeFunc() {
+  backDrop.style.display = "none";
+  cartModal.style.transform = "translateY(-100vh)";
+  cartModal.style.opacity = "0";
+}
+
+class Products {
+  getProducts() {
+    return productsData;
+  }
+}
+
+class Ui {
+  displayProducts(items) {
+    let result = "";
+    items.forEach((item) => {
+      result += `<div class="item">
+                  <img src=${item.imageUrl} alt="" loading="lazy">
+                  <section class="description">
+                      <p class="title">${item.title}</p>
+                      <p class="price">${item.price} Rs</p>
+                  </section>
+                  <button class="add-to-cart" data-id=${item.id}>
+                    add to cart
+                  </button>
+                </div>`;
+      productsParent.innerHTML = result;
+    });
+  }
+  getAddToCartBtn() {
+    const addToCartBtn = document.querySelectorAll(".add-to-cart");
+    const buttons = [...addToCartBtn];
+    domButtons = buttons;
+    if (cart.length === 0) {
+      cart = JSON.parse(localStorage.getItem("cart"));
+      if (cart === null) cart = [];
+    }
+    buttons.forEach((btn) => {
+      const id = btn.dataset.id;
+      const isInCart = cart.find((p) => p.id === parseInt(id));
+      if (isInCart) {
+        btn.innerText = "added";
+        btn.disabled = true;
+        btn.style.color = "#FB7185";
+      }
+      btn.addEventListener("click", (evt) => {
+        evt.target.innerText = "added";
+        evt.target.disabled = true;
+        evt.target.style.color = "#FB7185";
+        const addedProduct = { ...Storage.getProduct(id), quantity: 1 };
+        cart = [...cart, addedProduct];
+        Storage.saveCart(cart);
+        this.setCartValue(cart);
+        this.addCartItem(addedProduct);
+      });
+    });
+  }
+  setCartValue(cart) {
+    let tempCartItems = 0;
+    const totalPrice = cart.reduce((acc, curr) => {
+      tempCartItems += curr.quantity;
+      return acc + curr.quantity * curr.price;
+    }, 0);
+    cartTotal.innerText = `Total price: ${totalPrice.toFixed(2)} Rs`;
+    cartItems.innerText = tempCartItems;
+  }
+  addCartItem(cartItem) {
+    const div = document.createElement("div");
+    div.classList.add("cart-item");
+    div.innerHTML = `<img src=${cartItem.imageUrl} alt="" />
+                    <div class="cart-item-desc">
+                      <h4>${cartItem.title}</h4>
+                      <h5>${cartItem.price} Rs</h5>
+                    </div>
+                    
+                      <p>${cartItem.quantity}</p>
+                      <svg data-id=${cartItem.id}
+                        class="chevron-down"
+                        class="h-5 w-5"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                      </svg>
+                    </div>
+                    <svg data-id=${cartItem.id}
+                      class="trash"
+                      class="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>`;
+    cartContent.appendChild(div);
+  }
+  setupApp() {
+    cart = Storage.getCart() || [];
+    cart.forEach((cartItem) => this.addCartItem(cartItem));
+    this.setCartValue(cart);
+    this.cartLogic();
+  }
+  cartLogic() {
+    clearCart.addEventListener("click", () => {
+      cart.forEach((cItem) => this.removeItem(cItem.id));
+      while (cartContent.children.length) {
+        cartContent.removeChild(cartContent.children[0]);
+      }
+      closeFunc();
+    });
+    cartContent.addEventListener("click", (evt) => {
+      if (evt.target.classList.contains("chevron-up")) {
+        const addQuantity = evt.target;
+        const addedItem = cart.find(
+          (cItem) => cItem.id == addQuantity.dataset.id
+        );
+        addedItem.quantity++;
+        this.setCartValue(cart);
+        Storage.saveCart(cart);
+        addQuantity.nextElementSibling.innerText = addedItem.quantity;
+      } else if (evt.target.classList.contains("trash")) {
+        const removeItem = evt.target;
+        const removedItem = cart.find(
+          (cItem) => cItem.id == removeItem.dataset.id
+        );
+        this.removeItem(removedItem.id);
+        Storage.saveCart(cart);
+        cartContent.removeChild(removeItem.parentElement);
+      } else if (evt.target.classList.contains("chevron-down")) {
+        const subQuantity = evt.target;
+        const substractedItem = cart.find(
+          (cItem) => cItem.id == subQuantity.dataset.id
+        );
+        if (substractedItem.quantity === 1) {
+          this.removeItem(substractedItem.id);
+          cartContent.removeChild(subQuantity.parentElement.parentElement);
+          return;
+        }
+        substractedItem.quantity--;
+        this.setCartValue(cart);
+        Storage.saveCart(cart);
+        subQuantity.previousElementSibling.innerText = substractedItem.quantity;
+      }
+    });
+  }
+  removeItem(id) {
+    cart = cart.filter((cItem) => cItem.id !== id);
+    this.setCartValue(cart);
+    Storage.saveCart(cart);
+    const button = domButtons.find(
+      (btn) => parseInt(btn.dataset.id) === parseInt(id)
+    );
+    button.innerText = "add to cart";
+    button.disabled = false;
+    button.style.color = "#E11D48";
+  }
+}
+
+class Storage {
+  static saveProducts(datas) {
+    localStorage.setItem("products", JSON.stringify(datas));
+  }
+  static getProduct(id) {
+    const _products = JSON.parse(localStorage.getItem("products"));
+    return _products.find((p) => p.id === parseInt(id));
+  }
+  static saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+  static getCart() {
+    return JSON.parse(localStorage.getItem("cart"))
+      ? JSON.parse(localStorage.getItem("cart"))
+      : [];
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const products = new Products();
+  const productsData = products.getProducts();
+  const ui = new Ui();
+  ui.setupApp();
+  ui.displayProducts(productsData);
+  ui.getAddToCartBtn();
+  Storage.saveProducts(productsData);
+});
---
+++ shopping-cart-main/javascript/products.js
@@ -0,0 +1,32 @@
+export const productsData = [
+  {
+    id: 1,
+    title: "Bicycle",
+    price: 2000,
+    imageUrl: "./assets/images/cycle.webp",
+  },
+  {
+    id: 2,
+    title: "E-Scooter",
+    price: 45000,
+    imageUrl: "./assets/images/e-scooter.jpg",
+  },
+  {
+    id: 3,
+    title: "Bike",
+    price: 75000,
+    imageUrl: "./assets/images/bike.jpg",
+  },
+  {
+    id: 4,
+    title: "Car",
+    price: 194000,
+    imageUrl: "./assets/images/x-tron-car.jpg",
+  },
+  {
+    id: 5,
+    title: "Future Plant",
+    price: 3000,
+    imageUrl: "./assets/images/future-plant.jpg",
+  },
+];
